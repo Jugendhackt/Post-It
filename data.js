61dc3e5a500e67c0c1528b59424caaa06b0496aa
Jasper Michalke <jami@localhost.localdomain>
2017-06-11 13:03:05 +0200

    sg

--- data.js
+++ data.js
@@ -741,7 +741,7 @@ function NearestCity(latitude, longitude) {
   if (city[5] == "Thüringen") {number=15;}
   if (city[5] == "Berlin") {number=1;}
   
-  html = '<h3>Your next weather centre is '+city[1]+" in "+city[5]+". It's located "+city[4]+" meters above normal.</h3>";
+  html = '<h3>'++'</h3><h3>Your next weather centre is '+city[1]+" in "+city[5]+". It's located "+city[4]+" meters above normal.</h3>";
   html = html+'<img style="max-width:80%;height:auto;" src="https://maps.googleapis.com/maps/api/staticmap?size=500x400&markers=color:blue|'+city[2]+', '+city[3]+'|'+window.lat+', '+window.long+'"><h4>Statistics of frozen days per month. (Average of 1881 to 2010)</h4>';
   html = html+'<table>';
   html = html+'<tr><td style="font-weight:bold;width:30%;">jan</td><td><div style="width:'+jan*10+'%;height:30px;margin:-4px;margin-right:4px;float:left;background:#FF3529;"></div><span>'+jan+'</span></td></tr>';
